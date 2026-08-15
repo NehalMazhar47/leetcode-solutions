ffeccfe1fce00d8719029bbb63d3f12881e8ce5e
nehalmazhar0 <nehalmazar@gmail.com>
2026-08-15 16:55:03 +0530

    Remove duplicate Two Sum

--- cpp/0001.two-sum/solution.cpp
+++ cpp/0001.two-sum/solution.cpp
@@ -1,36 +1,54 @@
-// Created by nehalmazhar0 at 2026/08/15 16:08
-// leetgo: 1.4.17
-// https://leetcode.com/problems/two-sum/
-
-#include <bits/stdc++.h>
-#include "LC_IO.h"
-using namespace std;
-
-// @lc code=begin
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> arr;
+        for(int i=0;i<nums.size();i++){
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        int left=0;
+        int right=nums.size()-1;
+        while(left<right){
+            int sum = arr[left].first + arr[right].first;
+            if(sum==target){
+                return {arr[left].second, arr[right].second};;
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {};
+
         
     }
 };
 
-// @lc code=end
+View more
+ 
+More challenges
+. Number of Pairs of Strings With Concatenation Equal to Target
+. First Letter to Appear Twice
+. Find Subarrays With Equal Sum
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	stringstream out_stream;
 
-	vector<int> nums;
-	LeetCodeIO::scan(cin, nums);
-	int target;
-	LeetCodeIO::scan(cin, target);
+/5
+C++
 
-	Solution *obj = new Solution();
-	auto res = obj->twoSum(nums, target);
-	LeetCodeIO::print(out_stream, res);
-	cout << "\noutput: " << out_stream.rdbuf() << endl;
+Auto
 
-	delete obj;
-	return 0;
-}
+
+
+
+
+
+
+
+
+
+class Solution {public:    vector<int> twoSum(vector<int>&     nums, int target) {        vector<pair<int, int>> arr;        for(int i=0;i<nums.size();i++){            arr.push_back({nums[i], i});        }        sort(arr.begin(), arr.end());        int left=0;        int right=nums.size()-1;        while(left<right){            int sum = arr[left].first +             arr[right].first;            if(sum==target){                return {arr[left].                second, arr[right].                second};;
+Saved
+Upgrade to Cloud Saving
+Ln 1, Col 1
